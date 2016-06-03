0bfb8ca1a0e183a5f3c0300f7789af53dad6ffb5
Slava Knyazev <slava@knyz.org>
2016-06-02 20:34:27 -0400

    Added redirect following

--- basic.php
+++ basic.php
@@ -18,6 +18,7 @@ function GetUrl($url)
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
     $data = curl_exec($ch);
     curl_close($ch);
     return $data;
