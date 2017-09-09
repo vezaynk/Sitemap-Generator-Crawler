9d5691f4d895d47433a1206df4b7a337f60bee16
Slava Knyazev <slava@knyz.org>
2017-09-09 10:17:39 -0400

    Fixes #34

--- sitemap.php
+++ sitemap.php
@@ -90,6 +90,102 @@ function logger($message, $type)
     }
 }
 
+function flatten_url($url){
+    global $real_site;
+    $path = explode($real_site, $url)[1];
+    return $real_site . remove_dot_seg($path);
+}
+
+/**
+ * Remove dot segments from a URI path according to RFC3986 Section 5.2.4
+ * 
+ * @param $path
+ * @return string
+ * @link http://www.ietf.org/rfc/rfc3986.txt
+ */
+function remove_dot_seg($path) {
+    if (strpos($path, '.') === false) {
+        return $path;
+    }
+
+    $inputBuffer = $path;
+    $outputStack = [];
+
+    /**
+     * 2.  While the input buffer is not empty, loop as follows:
+     */
+    while ($inputBuffer != '') {
+        /**
+         * A.  If the input buffer begins with a prefix of "../" or "./",
+         *     then remove that prefix from the input buffer; otherwise,
+         */
+        if (strpos($inputBuffer, "./") === 0) {
+            $inputBuffer = substr($inputBuffer, 2);
+            continue;
+        }
+        if (strpos($inputBuffer, "../") === 0) {
+            $inputBuffer = substr($inputBuffer, 3);
+            continue;
+        }
+
+        /**
+         * B.  if the input buffer begins with a prefix of "/./" or "/.",
+         *     where "." is a complete path segment, then replace that
+         *     prefix with "/" in the input buffer; otherwise,
+         */
+        if ($inputBuffer === "/.") {
+            $outputStack[] = '/';
+            break;
+        }
+        if (substr($inputBuffer, 0, 3) === "/./") {
+            $inputBuffer = substr($inputBuffer, 2);
+            continue;
+        }
+
+        /**
+         * C.  if the input buffer begins with a prefix of "/../" or "/..",
+         *     where ".." is a complete path segment, then replace that
+         *     prefix with "/" in the input buffer and remove the last
+         *     segment and its preceding "/" (if any) from the output
+         *     buffer; otherwise,
+         */
+        if ($inputBuffer === "/..") {
+            array_pop($outputStack);
+            $outputStack[] = '/';
+            break;
+        }
+        if (substr($inputBuffer, 0, 4) === "/../") {
+            array_pop($outputStack);
+            $inputBuffer = substr($inputBuffer, 3);
+            continue;
+        }
+
+        /**
+         * D.  if the input buffer consists only of "." or "..", then remove
+         *     that from the input buffer; otherwise,
+         */
+        if ($inputBuffer === '.' || $inputBuffer === '..') {
+            break;
+        }
+
+        /**
+         * E.  move the first path segment in the input buffer to the end of
+         *     the output buffer, including the initial "/" character (if
+         *     any) and any subsequent characters up to, but not including,
+         *     the next "/" character or the end of the input buffer.
+         */
+        if (($slashPos = stripos($inputBuffer, '/', 1)) === false) {
+            $outputStack[] = $inputBuffer;
+            break;
+        } else {
+            $outputStack[] = substr($inputBuffer, 0, $slashPos);
+            $inputBuffer = substr($inputBuffer, $slashPos);
+        }
+    }
+
+    return ltrim(implode($outputStack), "/");
+}
+
 // Check if a URL has already been scanned
 function is_scanned($url)
 {
@@ -250,7 +346,7 @@ function get_links($html, $parent_url)
                     logger("URL is blacklisted. Rejecting.", 1);
                     return false;
                 }
-                return $href . ($query_string?'?'.$query_string:'');
+                return flatten_url($href . ($query_string?'?'.$query_string:''));
             }, $matches[2]);
             return $found;
         }
